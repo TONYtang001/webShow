fdd5a743af7391bb2ffe7d6e489231fb22dc300b
TONY <1872096136@qq.com>
2024-04-26 13:28:15 +0800

    commit1

---
+++ js/home.js
@@ -0,0 +1,16 @@
+document.querySelector('.login').addEventListener('click', ()=>{
+
+    
+    // 跳转到指定的 URL 地址(登录页面)
+    location.href = "login.html";//相对路径
+    // location.href = "D:/学校的杂七杂八/大二/大二下/杂/数据库课程设计/人员管理系统/html/login.html";
+    // 用反斜杠'\'可以实现字符串分行书写
+
+})
+
+document.querySelector('.register').addEventListener('click', ()=>{
+    
+    // 跳转到指定的 URL 地址(注册页面)
+    location.href = "register.html";//相对路径
+
+})
---
+++ js/login.js
@@ -0,0 +1,8 @@
+// username
+// password
+// btn
+document.querySelector('.btn').addEventListener('click',()=>{
+    const uname = document.querySelector('.username').value
+    const pword = document.querySelector('.password').value
+    console.log(uname+pword);
+})
