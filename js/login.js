ee8655111492eef36e90e1477d37cdb09bc56000
TONY <1872096136@qq.com>
2024-04-30 21:24:03 +0800

    update:a new page and link to existing pages

--- js/login.js
+++ js/login.js
@@ -5,4 +5,8 @@ document.querySelector('.btn').addEventListener('click',()=>{
     const uname = document.querySelector('.username').value
     const pword = document.querySelector('.password').value
     console.log(uname+pword);
+    // 判断登录输入的用户名密码是否符合数据库中查询到的数据
+    // 如果符合（if）
+    location.href = "./main.html";
+
 })
---
+++ js/register.js
@@ -0,0 +1,5 @@
+document.querySelector('.btn').addEventListener('click',()=>{
+    // 判断注册输入的用户名密码是否符合规定
+    // 如果符合（if）
+    location.href = "./login.html";
+})
