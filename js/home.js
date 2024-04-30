dc40a21243ca53bf91f866add561b2000e00cae6
TONY <1872096136@qq.com>
2024-04-30 15:05:22 +0800

    update_2024_4_30

--- js/home.js
+++ js/home.js
@@ -14,3 +14,8 @@ document.querySelector('.register').addEventListener('click', ()=>{
     location.href = "./html/register.html";//相对路径
 
 })
+
+setInterval(function() {  
+    document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);  
+  }, 2000);
+  
